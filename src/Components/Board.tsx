4afe066cd90f8f63b83aa4c91c117c6cc7b093e3
dkedid <73521518+pengoosedev@users.noreply.github.com>
2022-06-25 02:50:29 +0900

    Style

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -8,6 +8,8 @@ const Wrapper = styled.div`
     background-color: ${(props) => props.theme.boardColor};
     border-radius: 5px;
     min-height: 200px;
+    display: flex;
+    flex-direction: column;
 `;
 
 interface IBoardProps {
@@ -25,32 +27,47 @@ const Title = styled.div`
     border-radius: 3px;
 `;
 
-const WrapDiv = styled.div`
+interface IInfo {
+    isDraggingOver: boolean;
+    isDraggingFromThis: boolean;
+}
+const Area = styled.div<IInfo>`
     background: rgba(0, 0, 0, 0.3);
+    background: ${(props) =>
+        props.isDraggingOver
+            ? "#0398C2"
+            : props.isDraggingFromThis
+            ? "rgba(0,0,0,0.05)"
+            : "rgba(0,0,0,0.2)"};
     padding: 3px;
     border-radius: 3px;
+    margin-top: 10px;
+    flex-grow: 1;
+    transition: 0.15s ease-in-out;
 `;
+
 const Board = ({ toDos, boardId }: IBoardProps) => {
     return (
         <Wrapper>
             <Title>{boardId}</Title>
             <Droppable droppableId={boardId}>
-                {(provided) => (
-                    <Wrapper
+                {/*snapshot의 prop에 isDragOver은 드래그하고 있는게 현재 Droppable에 왔는지 확인 가능. */}
+                {(provided, info) => (
+                    <Area
+                        isDraggingOver={info.isDraggingOver}
+                        isDraggingFromThis={Boolean(info.draggingFromThisWith)}
                         ref={provided.innerRef}
                         {...provided.droppableProps}
                     >
-                        <WrapDiv>
-                            {toDos.map((toDo, index) => (
-                                <DragabbleCard
-                                    key={toDo}
-                                    index={index}
-                                    toDo={toDo}
-                                />
-                            ))}
-                            {provided.placeholder}
-                        </WrapDiv>
-                    </Wrapper>
+                        {toDos.map((toDo, index) => (
+                            <DragabbleCard
+                                key={toDo}
+                                index={index}
+                                toDo={toDo}
+                            />
+                        ))}
+                        {provided.placeholder}
+                    </Area>
                 )}
             </Droppable>
         </Wrapper>
--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -2,12 +2,18 @@ import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import React from "react";
 
-const Card = styled.div`
-    background: ${(props) => props.theme.cardColor};
+const Card = styled.div<{ isDragging: boolean }>`
+    background: ${(props) =>
+        props.isDragging ? "rgba(1,1,1,0.5)" : props.theme.cardColor};
+    color: ${(props) =>
+        props.isDragging ? "rgba(255, 255, 255, 0.5)" : "black"};
     padding: 10px;
     border-radius: 5px;
     font-weight: 600;
     margin: 5px;
+    box-shadow: ${(props) =>
+        props.isDragging ? "0px 0px 10px black" : "none"};
+    transition: 0.2s ease-in-out;
 `;
 
 interface IDraggable {
@@ -19,8 +25,9 @@ const DragabbleCard = ({ toDo, index }: IDraggable) => {
     console.log(toDo, "has been rendered");
     return (
         <Draggable key={toDo} draggableId={toDo} index={index}>
-            {(provided) => (
+            {(provided, snapshot) => (
                 <Card
+                    isDragging={snapshot.isDragging}
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
